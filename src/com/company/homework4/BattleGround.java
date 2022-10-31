2ccfe8ad229c662c21dc7b98ba3e347f3bf4c541
WhiteSnakeE <surtx0119@gmail.com>
2022-10-31 18:32:28 +0200

    added fourth homework

--- src/com/company/homework4/BattleGround.java
+++ src/com/company/homework4/BattleGround.java
@@ -1,5 +1,6 @@
 package com.company.homework4;
 
+import com.company.homework4.Heroes.Archer;
 import com.company.homework4.Heroes.Hero;
 import com.company.homework4.Heroes.Mage;
 import com.company.homework4.Heroes.Warrior;
@@ -13,17 +14,17 @@ public class BattleGround {
     public static void main(String[] args) {
         Enemy skeleton  = new Skeleton("Skeleton",200);
         Enemy smallSkeleton  = new Skeleton("Small Skeleton",150);
-        Hero mage = new Mage("Mage",500);
-        while(mage.isAlive()){
+        Hero archer = new Archer("Hunter",500);
+        while(archer.isAlive()){
             if(skeleton.isAlive()){
-                mage.attackEnemy(skeleton);
-                skeleton.attack(mage);
-                mage.strengthAttack(skeleton);
+                archer.attackEnemy(skeleton);
+                skeleton.attack(archer);
+                archer.strengthAttack(skeleton);
             }
             if(smallSkeleton.isAlive()){
-                mage.attackEnemy(smallSkeleton);
-                smallSkeleton.attack(mage);
-                mage.strengthAttack(smallSkeleton);
+                archer.attackEnemy(smallSkeleton);
+                smallSkeleton.attack(archer);
+                archer.strengthAttack(smallSkeleton);
             }
         }
 
--- src/com/company/homework4/TrainingGround.java
+++ src/com/company/homework4/TrainingGround.java
@@ -1,7 +1,15 @@
 package com.company.homework4;
 
+import com.company.homework4.Heroes.Hero;
+import com.company.homework4.Heroes.Mage;
+import com.company.homework4.enemy.Enemy;
+import com.company.homework4.enemy.Skeleton;
+
 public class TrainingGround {
     public static void main(String[] args) {
-
+        Enemy skeleton  = new Skeleton("Skeleton",200);
+        Hero mage = new Mage("Mage",500);
+        mage.attackEnemy(skeleton);
+        skeleton.attack(mage);
     }
 }
